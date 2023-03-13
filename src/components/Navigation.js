b2cb4e0f3d42a4f9433bc5b64efdbdfdcb97c973
AlbinBekovic1 <85638477+AlbinBekovic1@users.noreply.github.com>
2023-03-13 16:14:08 +0100

    4 files modified, 2 added

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import '../styles/navigation.scss';
 
 const navigation = () => {
   return (
-    <div>
+    <div class="header">
       <nav class="nav">
         <NavLink to="/utilisateurs/:user_id"> Detail </NavLink>
         <br />
--- src/views/liste.js
+++ src/views/liste.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/liste.scss';
+import React, { useEffect, useState } from 'react';
+import React, { NavLink } from 'react-router-dom';
 
 const liste = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    if (!users.lenght) {
+      fetch('https://randomuser.me/api/?results=10')
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          setUsers(data.results);
+          localStorage.setItem('liste', JSON.stringify(data.results));
+        });
+    }
+  }, [users]);
+
   return (
-    <div>
+    <nav className="users">
       <h1>Liste des utilisateurs</h1>
-      <p>Ceci est une liste</p>
-      <Link to="/utilisateurs/editer/:user_id"> Gestion des utilisiateurs</Link>
-      <Link to="/utilisateurs/:user_id"> Détails des utilisiateurs</Link>
-    </div>
+      <div className="grid">
+        {users.map((e) => (
+          <div className="user">
+            <img src={e.picture.thumbnail} /> <br />
+            <span>
+              {e.name.title} {e.name.first} {e.name.last}
+            </span>
+            <br />
+            <span>Nationalité : {e.nat}</span> <br />
+            <NavLink to={e.email}>{e.email}</NavLink> <br />
+          </div>
+        ))}
+      </div>
+    </nav>
   );
 };
 
